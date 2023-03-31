197a46c563503df539889f448d1fc9490f76bfe5
tammytran96 <tammytran96@gmail.com>
2023-03-31 12:53:26 -0700

    Story 4 completed, added specs to animals and sightings

--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Animal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    crocodile = Animal.create(common_name: 'Crocodile', scientific_binomial: 'Crocodylidae')
+    expect(crocodile).to be_valid
+    end
+  
+    it 'is not valid without a common name' do
+      crocodile = Animal.create(scientific_binomial: 'Crocodylidae')
+      expect(crocodile.errors[:common_name]).to_not be_empty
+    end
+
+    it 'is not valid without a scientific binomial' do
+      crocodile = Animal.create(common_name: 'Crocodile')
+      expect(crocodile.errors[:scientific_binomial]).to_not be_empty
+    end
+
+    it 'is not valid if common name is not unique' do
+      Animal.create(common_name: 'Crocodile', scientific_binomial: 'Crocodylidae')
+      crocodile = Animal.create(common_name: 'Crocodile', scientific_binomial: 'Crocodylidae')
+      expect(crocodile.errors[:common_name]).to_not be_empty
+    end
+
+    it 'is not valid if scientific binomial is not unique' do
+      Animal.create(common_name: 'Crocodile', scientific_binomial: 'Crocodylidae')
+      crocodile = Animal.create(common_name: 'Crocodile', scientific_binomial: 'Crocodylidae')
+      expect(crocodile.errors[:scientific_binomial]).to_not be_empty
+    end
+
+    it 'is not valid if common name exactly matches scientific binomial' do
+      crocodile = Animal.create(common_name: 'Crocodile', scientific_binomial: 'Crocodile')
+      expect(crocodile.errors[:common_name]).to_not be_empty
+    end
+
 end
--- spec/models/sighting_spec.rb
+++ spec/models/sighting_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Sighting, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it 'is not valid without a latitude' do
+      sighting = Sighting.create(longitude: 74, date: "2023-02-02")
+      expect(sighting.errors[:latitude]).to_not be_empty
+    end
+
+    it 'is not valid without a longitude' do
+      sighting = Sighting.create(latitude: 74, date: "2023-02-02")
+      expect(sighting.errors[:longitude]).to_not be_empty
+    end
+
+    it 'is not valid without a date' do
+      sighting = Sighting.create(latitude: 74, longitude: 74)
+      expect(sighting.errors[:date]).to_not be_empty
+    end
 end
